8fbcbf8bf6d6a1cf9e7f236e12738450a3eb3de7
Meredith Roman <meredith.c.roman@gmail.com>
2016-05-22 13:54:25 -0400

    javascript finished

--- script.js
+++ script.js
@@ -1,31 +1,75 @@
 /* **** Guessing Game Functions **** */
 
-// Generate the Winning Number
-function generateWinningNumber(){
-    var rand = Math.random() * 100;
+function gameRunner() {
+    // generate the winning number
+    var generateNumber = function () {
+        var rand = Math.random() * 100;
+        return Math.ceil(rand);
+    }
+
+    winningNumber = generateNumber();
+
+    console.log(winningNumber);
+    
+    // to store guesses
     var prevGuesses = []
 
-    // round winning number up to a whole number
-    var winningNumber = Math.ceil(rand);
-    
-    console.log(winningNumber);
+    var savedHint = false;
+
+    // Determine if the next guess should be a lower or higher number
+    function lowerOrHigher(playersGuess){
+        var distance = Math.abs(winningNumber - playersGuess);
+        distance = Math.ceil(distance / 10) * 10;
+        var direction;
+
+        if (playersGuess > winningNumber) {
+            direction = "higher";
+        } else if (playersGuess < winningNumber) {
+            direction = "lower";
+        }
+
+        return "Your guess is " + direction + " and less than " + distance + " digits away from the Winning Number!"
+    }
 
     // Check if the Player's Guess is the winning number 
     function checkGuess(playersGuess) {
         if (playersGuess == winningNumber) {
             $('#message').text('Congratulations, you won!')
         } else {
-            $('#message').text('Your guess was incorrect, please try again.')
             // check if player has guessed this number before
             // JQuery.inArray returns -1 if an item is not present in an array
             var repeatGuess = $.inArray(playersGuess, prevGuesses) > -1;
-            if (!repeatGuess) {
+            if (repeatGuess) {
+                $('#message').text("You already guessed that number, silly.");
+                $('#message').css({'color': 'red', 'font-weight': 'bold'});
+            } else if (prevGuesses.length == 4) { // if this is your 5th guess
+                $('#message').text("Sorry, you lost :'(");
+            } else {
                 prevGuesses.push(playersGuess);
+                message = lowerOrHigher(playersGuess);
+                $('#message').text(message);
             }
         }
     }
 
-    return checkGuess;
+    // Provide a hint to the player
+    function provideHint(){
+        if (!savedHint) {
+            var hint = [winningNumber, generateNumber(), generateNumber()];
+            // sort CORRECTLY
+            // js sorts elements alphabetically, which means 8 would come after 22
+            // WHY JAVASCRIPT WHY??
+            hint.sort(function(a, b){return a - b});
+            savedHint = "One of these values is the winning number: " + hint.join(", ");
+        }
+        $('#message').text(savedHint);
+    }
+
+    // return the hint and guess functions
+    return {
+        provideHint: provideHint,
+        checkGuess: checkGuess
+    }
 
 }
 
@@ -41,40 +85,33 @@ function playersGuessSubmission(input) {
         input.val("");
         return Number(playersGuess);
     } else {
+        $('#message').text("Please guess a number between 1 and 100:")
         $('#message').css({'color': 'red', 'font-weight': 'bold'});
         return false;
     }
 }
 
-// Determine if the next guess should be a lower or higher number
-
-function lowerOrHigher(){
-	// add code here
-}
-
-// Create a provide hint button that provides additional clues to the "Player"
-
-function provideHint(){
-	// add code here
-}
-
 /* **** Event Listeners/Handlers **** */
 
 $(document).ready(function() {
-    // generate winning number and get checkGuess function
-    // probably want to wrap this all in a game running function
-    var checkGuess = generateWinningNumber();
-
-    
+    var game = gameRunner();
+    var checkGuess = game.checkGuess;
+    var provideHint = game.provideHint  ;  
     
     $('#guess').on('click', function(e) {
         e.preventDefault();
         var playersGuess = playersGuessSubmission($('#enter-number'));
         // if the player has guessed a possible number
         if (playersGuess) {
-            checkGuess(playersGuess)
+            checkGuess(playersGuess);
         }
-        
+    });
+
+    $('#hint').on('click', function(e) {
+        e.preventDefault();
+        // make sure css is correct
+        $('#message').css({'color': 'inherit', 'font-weight': 'inherit'});
+        provideHint();
     });
 
     // clicking the restart button reloads the page, resetting the game
